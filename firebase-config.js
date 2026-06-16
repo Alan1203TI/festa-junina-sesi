fec52fe8924e017aceaeb1550810f00ed9388b0f
Alan1203TI <167146297+Alan1203TI@users.noreply.github.com>
2026-06-16 20:39:06 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD1ml_HSpqPOB8hk4i5BOuFRTzP0NmSoKU",
-  authDomain: "festa-junina-sesi.firebaseapp.com",
-  projectId: "festa-junina-sesi",
-  storageBucket: "festa-junina-sesi.firebasestorage.app",
-  messagingSenderId: "37375577335",
-  appId: "1:37375577335:web:6f5b2f3e310a47acd3d7f2"
-};
